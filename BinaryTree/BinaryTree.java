5fe5d419a6fadbca30204d9c8ec33d599d19b013
Rohan Ray Yadav <rohanyadav.fb@gmail.com>
2023-10-23 14:39:51 -0500

    test

--- BinaryTree/BinaryTree.java
+++ BinaryTree/BinaryTree.java
@@ -1,4 +1,16 @@
 
 public class BinaryTree {
-    
+    private Node root;
+
+    public BinaryTree(){
+        this.root = new Node(10);
+    }
+
+    public boolean addLeft(){}
+
+    public setRoot(){}
+
+    public boolean addRight(){}
+
+    public boolean remove(){}
 }
